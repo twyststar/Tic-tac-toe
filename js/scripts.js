db7732bcd7329b733fefc40c4899597845ec0326
Marcel Nogueira and Starlynne Start <starlynne@>
2017-04-06 14:00:58 -0700

    Add table and logic to gather cell clicked value from table

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+//business logic
+
+
+
+
+
+
+//UI logic
+$(document).ready(function(){
+  $("button").click(function(){
+    event.preventDefault();
+    var sqVal = $(this).val();
+    
+
+
+
+
+    console.log(sqVal);
+  });
+});
